d7bcf75f35b75f11d600d4ecefd6d24117eb7747
soleshhaker <tobi1560@t.pl>
2020-07-02 14:24:02 +0200

    add jump ugrade notification

---
+++ pierwsza gra/Assets/scripts/upgradetext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class upgradetext : MonoBehaviour
+{
+    public static string upgradestatus;
+    public TextMeshProUGUI upgrade;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        upgrade = GetComponent<TextMeshProUGUI>();
+        upgradestatus = "Press enter to upgrade jump ability for 300 points";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        upgrade.text = upgradestatus;
+    }
+}
