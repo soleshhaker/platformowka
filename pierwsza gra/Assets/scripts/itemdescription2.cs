73bcbe1b2bd4d71b92bd9843b5507ca731aff390
soleshhaker <tobi1560@t.pl>
2020-07-06 14:01:49 +0200

    make speedpotion last for 30 seconds

--- pierwsza gra/Assets/scripts/itemdescription2.cs
+++ pierwsza gra/Assets/scripts/itemdescription2.cs
@@ -11,17 +11,29 @@ public class itemdescription2 : MonoBehaviour
     GameObject objToSpawn;
     GameObject ui;
     int istext;
+    float speed;
     public TextAlignmentOptions alignment { get; set; }
 
     void Awake()
     {
         ui = GameObject.Find("status");
         int istext = 0;
+        speed = speedpotion.speedvalue;
+        Debug.Log(speed);
     }
 
+    void usepotion(float speed)
+    {
+        CharacterControls.speed += speed;
+    }
+
+    void endpotion(float speed)
+    {
+        CharacterControls.speed -= speed;
+    }
     void Update()
     {
-        Debug.Log(inventory.speedpotionobtained);
+        
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -35,7 +47,7 @@ public class itemdescription2 : MonoBehaviour
             objToSpawn.transform.position = new Vector3(300, 0, 3);
             objToSpawn.GetComponent<RectTransform>().localPosition = new Vector3(10, -170, 1);
             objToSpawn.GetComponent<RectTransform>().sizeDelta = new Vector2(400, 60);
-            objToSpawn.GetComponent<Text>().text = "Potion than enhances speed (Click to use)";
+            objToSpawn.GetComponent<Text>().text = "Potion than enhances speed, lasts for 30 seconds (Click to use)";
             objToSpawn.GetComponent<Text>().font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
             istext = 1;
 
@@ -57,7 +69,10 @@ public class itemdescription2 : MonoBehaviour
         Debug.Log("Pointer Click");
 
         objToSpawn.GetComponent<Text>().text = "Potion used!";
-        CharacterControls.speed += 2;
+        usepotion(speed);
+        Debug.Log(speed);
+
+        Invoke("endpotion(speed)", 30);
         inventory.speedpotionobtained -= 1;
         istext = 1;
         if (inventory.speedpotionobtained == 0)
--- pierwsza gra/Assets/scripts/speedpotion.cs
+++ pierwsza gra/Assets/scripts/speedpotion.cs
@@ -7,11 +7,18 @@ public class speedpotion : MonoBehaviour
 {
     static public GameObject spdpotion;
     static public GameObject speedpotionslot;
+    static public float speedvalue;
 
+    void Awake()
+    {
+        speedvalue = 2f;
+
+    }
     void Start()
     {
         spdpotion = GameObject.Find("Image2");
         speedpotionslot = GameObject.Find("InventorySlot2");
+       
     }
 
     void OnTriggerEnter(Collider col)
