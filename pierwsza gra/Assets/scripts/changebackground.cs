b847409fbc2eb9673c756aab405153ac450dce4c
soleshhaker <tobi1560@t.pl>
2020-07-14 18:07:02 +0200

    add changing backgrounds
    to do: add fade in and fade out effect to backgrounds

---
+++ pierwsza gra/Assets/scripts/changebackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changebackground : MonoBehaviour
+{
+
+    private SpriteRenderer tlo;
+    [SerializeField] private Sprite tlo2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tlo = GameObject.Find("tlo").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            tlo.sprite = tlo2;
+
+            Debug.Log("zmianatla");
+        }
+    }
+ 
+}
---
+++ pierwsza gra/Assets/scripts/changebackground2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changebackground2 : MonoBehaviour
+{
+
+    private SpriteRenderer tlo;
+    [SerializeField] private Sprite tlo1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tlo = GameObject.Find("tlo").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            tlo.sprite = tlo1;
+
+            Debug.Log("zmianatla");
+        }
+    }
+}
+ 
