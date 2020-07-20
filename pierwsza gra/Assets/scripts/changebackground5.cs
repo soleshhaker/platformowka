c2ee2158e203c204d486df672f61a9f135cd6e29
soleshhaker <tobi1560@t.pl>
2020-07-20 17:56:39 +0200

    add tower

---
+++ pierwsza gra/Assets/scripts/changebackground5.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changebackground5 : MonoBehaviour
+{
+
+    private SpriteRenderer tlo;
+
+    void destroy()
+    {
+        Destroy(gameObject, 2f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        tlo = GameObject.Find("tlo2").GetComponent<SpriteRenderer>();
+
+
+        Color c = tlo.material.color;
+
+        c.a = 0f;
+
+        tlo.material.color = c;
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            StartCoroutine("FadeOut");
+            Debug.Log("zmianatla");
+            destroy();
+        }
+    }
+    IEnumerator FadeOut()
+
+    {
+
+        for (float f = 0f; f <= 1f; f += 0.05f)
+
+        {
+
+            Color c = tlo.material.color;
+
+            c.a = f;
+
+            tlo.material.color = c;
+
+            yield return new WaitForSeconds(0.05f);
+
+        }
+
+    }
+}
---
+++ pierwsza gra/Assets/scripts/changebackground6.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changebackground6 : MonoBehaviour
+{
+
+    private SpriteRenderer tlo;
+
+    void destroy()
+    {
+        Destroy(gameObject, 2f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        tlo = GameObject.Find("tlo3").GetComponent<SpriteRenderer>();
+
+
+        Color c = tlo.material.color;
+
+        c.a = 1f;
+
+        tlo.material.color = c;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            StartCoroutine("FadeOut");
+            Debug.Log("zmianatla2");
+            destroy();
+        }
+    }
+    IEnumerator FadeOut()
+
+    {
+
+        for (float f = 1f; f >= -0.05f; f -= 0.05f)
+
+        {
+
+            Color c = tlo.material.color;
+
+            c.a = f;
+
+            tlo.material.color = c;
+
+            yield return new WaitForSeconds(0.05f);
+
+        }
+    }
+}
