d43e13b71c6fbd8d49c2a8f65eea70b87f2a8cab
soleshhaker <tobi1560@t.pl>
2020-09-04 12:46:35 +0200

    add castle assets and entrance to the castle with camer change

---
+++ pierwsza gra/Assets/scripts/camerachangelvl3.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using Cinemachine;
+using System.Security.Cryptography;
+
+public class camerachangelvl3 : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject vcam1;
+    public GameObject vcam2;
+
+
+    void Start()
+    {
+        player = GameObject.Find("player");
+        vcam1 = GameObject.Find("vcam1");
+        vcam2 = GameObject.Find("vcam2");
+        vcam2.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+
+            vcam1.SetActive(false);
+            vcam2.SetActive(true);
+
+        }
+
+
+
+    }
+
+}
