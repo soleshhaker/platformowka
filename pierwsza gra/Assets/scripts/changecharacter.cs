0a6046172644c8c384de0cfe4d2cb758fcee3ab6
soleshhaker <tobi1560@t.pl>
2020-09-10 19:40:15 +0200

    add button to change character
    need to fix camera and animations for new character

---
+++ pierwsza gra/Assets/scripts/changecharacter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class changecharacter : MonoBehaviour
+    , IPointerClickHandler
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject character;
+    [SerializeField] private GameObject player;
+    Vector3 position;
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        position = player.transform.position;
+        Destroy(player, 0.1f);
+
+        Instantiate(character, position, Quaternion.identity);
+    }
+}
